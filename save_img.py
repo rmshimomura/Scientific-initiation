4d40020bedf0a96771c1a3dcba31861b974c7292
rmshimomura <shimomurarodrigo1@gmail.com>
2022-11-09 15:22:40 -0300

    Change to class

--- save_img.py
+++ save_img.py
@@ -5,6 +5,12 @@ from shapely.geometry import Point
 import os
 import datetime
 
+class Infection_Circle:
+    
+    def __init__(self, index, circle, buffer):
+        self.index = index
+        self.circle = circle
+        self.buffer = buffer
 
 for filename in os.listdir('Results'):
     os.remove(f'Results/{filename}')
@@ -18,7 +24,6 @@ for i in range(0, len(_collectors)):
     # print _collectors["Data"].iloc[i] only if the value is not nan
     if not pd.isnull(_collectors["Data_1o_Esporos"].iloc[i]):
         _collectors.loc[i, 'Data_1o_Esporos'] = _collectors["Data_1o_Esporos"].iloc[i].strftime('%Y-%m-%d')
-        print(_collectors["Data_1o_Esporos"].iloc[i])
 
 
 # print(_collectors["Data_1o_Esporos"].iloc[0] + datetime.timedelta(days=2))
@@ -35,6 +40,41 @@ def coloring():
     for i in first_apperances.index: # Color the first collectors to appear in yellow
         _collectors.loc[i, 'color'] = 'yellow'
 
+# def growth(number_of_days):
+
+#     global infection_circles
+
+#     infection_circles = []
+
+#     for i in range(len(first_apperances)): # For each starting point
+
+#         infection_circle = Point(first_apperances['Longitude'].iloc[i], first_apperances['Latitude'].iloc[i]).buffer(0.01 * 1) # Create a new infection circle
+#         # infection_circles.append([i, infection_circle, 1]) # Add the infection circle to the list
+#         infection_circles.append(Infection_Circle(i, infection_circle, 1))
+        
+#     for day in range(number_of_days): # For each day
+
+#         for infection_circle_index in range(len(infection_circles)): # For each infection circle
+
+#             for collector_index in range(len(_collectors)): # For each collector
+
+#                 current_color = _collectors['color'].iloc[collector_index]
+
+#                 if current_color == 'yellow' or current_color == 'red': # Prevent re-infection and collectors that don't have spores
+#                     continue
+#                 else:
+#                     if infection_circles[infection_circle_index][1].contains(Point(_collectors['Longitude'].iloc[collector_index], _collectors['Latitude'].iloc[collector_index])):
+#                         _collectors.loc[collector_index, 'color'] = 'yellow'
+#                         new_buffer = 1
+#                         new_infection_circle = Point(_collectors['Longitude'].iloc[collector_index], _collectors['Latitude'].iloc[collector_index]).buffer(0.01 * new_buffer)
+#                         infection_circles.append([collector_index, new_infection_circle, new_buffer])
+
+#         for infection_circle_index in range(len(infection_circles)): # For each day passed, the infection circle grows
+#             infection_circles[infection_circle_index][2] += 1
+#             infection_circles[infection_circle_index][1] = Point(infection_circles[infection_circle_index][1].centroid.x, infection_circles[infection_circle_index][1].centroid.y).buffer(0.01 * infection_circles[infection_circle_index][2])
+
+#         plotting(day)
+
 def growth(number_of_days):
 
     global infection_circles
@@ -44,30 +84,32 @@ def growth(number_of_days):
     for i in range(len(first_apperances)): # For each starting point
 
         infection_circle = Point(first_apperances['Longitude'].iloc[i], first_apperances['Latitude'].iloc[i]).buffer(0.01 * 1) # Create a new infection circle
-        infection_circles.append([i, infection_circle, 1]) # Add the infection circle to the list
-        
+        infection_circles.append(Infection_Circle(i, infection_circle, 1))
+
     for day in range(number_of_days): # For each day
 
-        for infection_circle_index in range(len(infection_circles)): # For each infection circle
+        for _infection_circle in infection_circles:
 
-            for collector_index in range(len(_collectors)): # For each collector
+            for collector_index in range(len(_collectors)):
 
                 current_color = _collectors['color'].iloc[collector_index]
 
                 if current_color == 'yellow' or current_color == 'red': # Prevent re-infection and collectors that don't have spores
                     continue
                 else:
-                    if infection_circles[infection_circle_index][1].contains(Point(_collectors['Longitude'].iloc[collector_index], _collectors['Latitude'].iloc[collector_index])):
+                    if _infection_circle.circle.contains(Point(_collectors['Longitude'].iloc[collector_index], _collectors['Latitude'].iloc[collector_index])):
                         _collectors.loc[collector_index, 'color'] = 'yellow'
                         new_buffer = 1
                         new_infection_circle = Point(_collectors['Longitude'].iloc[collector_index], _collectors['Latitude'].iloc[collector_index]).buffer(0.01 * new_buffer)
-                        infection_circles.append([collector_index, new_infection_circle, new_buffer])
+                        infection_circles.append(Infection_Circle(collector_index, new_infection_circle, new_buffer))
 
-        for infection_circle_index in range(len(infection_circles)): # For each day passed, the infection circle grows
-            infection_circles[infection_circle_index][2] += 1
-            infection_circles[infection_circle_index][1] = Point(infection_circles[infection_circle_index][1].centroid.x, infection_circles[infection_circle_index][1].centroid.y).buffer(0.01 * infection_circles[infection_circle_index][2])
+        for _infection_circle in infection_circles: # For each day passed, the infection circle grows
+
+            _infection_circle.buffer += 1
+            _infection_circle.circle = Point(_infection_circle.circle.centroid.x, _infection_circle.circle.centroid.y).buffer(0.01 * _infection_circle.buffer)
 
         plotting(day)
+
 
 def plotting(day):
 
@@ -101,7 +143,7 @@ def plot_infection_circles():
 
     for i in range(len(infection_circles)):
 
-        old_circles.append(plt.plot(*infection_circles[i][1].exterior.xy, color='black', linewidth=1))
+        old_circles.append(plt.plot(*infection_circles[i].circle.exterior.xy, color='black', linewidth=1))
 
 def intersection_union():
 
@@ -110,8 +152,8 @@ def intersection_union():
 
     while i < len(infection_circles):
         while j < len(infection_circles):
-            if infection_circles[i][1].intersects(infection_circles[j][1]):
-                infection_circles[i][1] = infection_circles[i][1].union(infection_circles[j][1])
+            if infection_circles[i].circle.intersects(infection_circles[j].circle):
+                infection_circles[i].circle = infection_circles[i].circle.union(infection_circles[j].circle)
                 infection_circles.pop(j)
             else:
                 j += 1
