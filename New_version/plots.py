145bd3114e9c043e5fcad674687600f026e9aed2
rmshimomura <shimomurarodrigo1@gmail.com>
2022-12-20 20:02:57 -0300

    Saving figure in .svg

--- New_version/plots.py
+++ New_version/plots.py
@@ -3,6 +3,8 @@ import datetime
 import utils
 import geopandas as gpd
 import pandas as pd
+from IPython.display import set_matplotlib_formats
+set_matplotlib_formats('svg')
 
 
 def plot_infection_circles(infection_circles: list, old_circles: list) -> None:
@@ -12,12 +14,12 @@ def plot_infection_circles(infection_circles: list, old_circles: list) -> None:
 
 def plotting(_map: gpd.GeoDataFrame, _collectors: pd.DataFrame, infection_circles: list, old_circles: list, start_day: datetime.date, day: int) -> None:
 
-    if day == 0:
-        _map.plot(color='lightgrey', edgecolor='black')
-        plt.xlabel('Longitude', fontsize=14)
-        plt.ylabel('Latitude', fontsize=14)
+    # if day == 0:
+    _map.plot(color='lightgrey', edgecolor='grey', linewidth=0.5)
+    plt.xlabel('Longitude', fontsize=14)
+    plt.ylabel('Latitude', fontsize=14)
 
-        utils.show_detected_collectors_city_names(_collectors, plt)
+    utils.show_detected_collectors_city_names(_collectors, plt)
 
     for old_circle in old_circles:
         old_circle[0].remove()
@@ -28,4 +30,7 @@ def plotting(_map: gpd.GeoDataFrame, _collectors: pd.DataFrame, infection_circle
     
     plt.title(f"Ferrugem asiática no Paraná - dia {(start_day + datetime.timedelta(day)).strftime('%Y-%m-%d')} ({day})", fontsize=20)
     plt.scatter(_collectors['Longitude'], _collectors['Latitude'], color=_collectors['color'], s=100, marker='.')
-    plt.pause(0.01)
+    
+    plt.tight_layout()
+    plt.gcf().set_size_inches(40, 30)
+    plt.savefig(f'G:/My Drive/IC/Codes/Plots/plot_{day}.svg')
