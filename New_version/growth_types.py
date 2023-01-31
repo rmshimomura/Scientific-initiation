f95630690966333ce574dbe0849638733afd7268
rmshimomura <shimomurarodrigo1@gmail.com>
2023-01-30 22:26:07 -0300

    Prototype penalty?

--- New_version/growth_types.py
+++ New_version/growth_types.py
@@ -8,12 +8,27 @@ difference_values = []
 total_error = 0
 invalid_collectors = 0
 
-def check_day(current_day: int, collector: pd.DataFrame, TEST_PARAMS: dict) -> None:
+def check_day(_collectors: pd.DataFrame, current_day: int, collector: pd.DataFrame, TEST_PARAMS: dict) -> None:
 
     global total_error, difference_values, invalid_collectors
 
     if pd.isnull(collector.Data_1o_Esporos):
         # total_error += TEST_PARAMS['collector_penalty']**2
+
+        closest_collectors = []
+
+        for i in _collectors.itertuples():
+            if not pd.isnull(i.Data_1o_Esporos):
+                closest_collectors.append(i)
+
+        closest_collectors.sort(key=lambda x: Point(x.Longitude, x.Latitude).distance(Point(collector.Longitude, collector.Latitude)))
+
+        closest_collectors = closest_collectors[0]
+
+        print(f"Closest: {Point(closest_collectors.Longitude, closest_collectors.Latitude).distance(Point(collector.Longitude, collector.Latitude)) * 111.045}km")
+        print(f"Days difference: {(current_day - closest_collectors.Data_1o_Esporos).days} days\n")
+        print(f"Penalty applied: {math.abs((current_day - closest_collectors.Data_1o_Esporos).days)}")
+        # If the days are positive, put the number of days needed to reach the specific collector?
         invalid_collectors += 1
         return
 
@@ -67,14 +82,14 @@ def circular_growth(_map: gpd.GeoDataFrame, _collectors: pd.DataFrame, first_app
 
                                 _collectors.loc[collector.Index, 'color'] = 'red'
 
-                            check_day(start_day + datetime.timedelta(day), collector, TEST_PARAMS)
+                            check_day(_collectors, start_day + datetime.timedelta(day), collector, TEST_PARAMS)
 
                             new_infection_circle = Point(collector.Longitude, collector.Latitude)
                             new_infection_circle.buffer(TEST_PARAMS['buffer_factor'])
                             infection_circles.append(Infection_Circle(new_infection_circle, 1))
         
         for infection_circle in infection_circles:
-            infection_circle.grow(TEST_PARAMS['buffer_factor'], TEST_PARAMS['expansion_threshold'])
+            infection_circle.grow(TEST_PARAMS['buffer_factor'], day)
 
         if day == TEST_PARAMS['number_of_days'] - 1:
             plots.plotting(_map, _collectors, infection_circles, old_circles, start_day, day)
