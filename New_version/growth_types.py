76975efd0fa97ab7183ddfc01f8ae2a4130a6123
rmshimomura <shimomurarodrigo1@gmail.com>
2023-02-06 18:45:24 -0300

    Updating penalty

--- New_version/growth_types.py
+++ New_version/growth_types.py
@@ -3,59 +3,28 @@ import geopandas as gpd
 import datetime, plots, math
 from shapely.geometry import Point
 from infection_circle import Infection_Circle
+import growth_functions as gf
 
 difference_values = []
 total_error = 0
 invalid_collectors = 0
 
-def check_day(_collectors: pd.DataFrame, current_day: int, collector: pd.DataFrame, TEST_PARAMS: dict) -> None:
+def check_day(collector: pd.DataFrame, current_day: int) -> None:
 
     global total_error, difference_values, invalid_collectors
 
-    if pd.isnull(collector.Data_1o_Esporos):
-        # total_error += TEST_PARAMS['collector_penalty']**2
+    if pd.isnull(collector.Data_1o_Esporos): # False positive
 
-        closest_collectors = []
-
-        for i in _collectors.itertuples():
-            if not pd.isnull(i.Data_1o_Esporos):
-                closest_collectors.append(i)
-
-        closest_collectors.sort(key=lambda x: Point(x.Longitude, x.Latitude).distance(Point(collector.Longitude, collector.Latitude)))
-
-        closest_collectors = closest_collectors[0]
-
-        print(f"Closest: {Point(closest_collectors.Longitude, closest_collectors.Latitude).distance(Point(collector.Longitude, collector.Latitude)) * 111.045}km")
-        print(f"Days difference: {(current_day - closest_collectors.Data_1o_Esporos).days} days\n")
-        print(f"Penalty applied: {abs((current_day - closest_collectors.Data_1o_Esporos).days)}")
-        '''
-
-            Dias positivos = Demorou para chegar?
-            Dias negativos = Chegou muito cedo?
-
-            Penalizar distância e tempo?
-
-            Máximo de dias - dia da descoberta para falsos positivos?
-
-        '''
-        invalid_collectors += 1
+        collector.discovery_day = current_day
+        
         return
 
-    difference = (current_day - collector.Data_1o_Esporos).days
+    else: # True positive
+        difference = abs((current_day - collector.Data_1o_Esporos).days)
 
-    difference_values.append(difference)
+        difference_values.append(difference)
 
-    total_error += difference**2
-
-    if current_day == TEST_PARAMS['end_day']:
-        difference_log = open(f'G:/Meu Drive/IC/Codes/Logs/difference_log_{TEST_PARAMS["number_of_days"]}_{TEST_PARAMS["buffer_factor"]}.txt', 'w')
-
-        for value in difference_values:
-            difference_log.write(f'Difference: {value} days\n')
-
-        difference_log.write(f'\nMean: {sum(difference_values)/len(difference_values)}, Standard deviation: {math.sqrt(sum(difference_values)/len(difference_values))}')
-
-        difference_log.close()
+        total_error += difference**2
 
 
 def circular_growth(_map: gpd.GeoDataFrame, _collectors: pd.DataFrame, first_apperances: pd.DataFrame, old_circles: list, TEST_PARAMS: dict) -> int:
@@ -65,9 +34,14 @@ def circular_growth(_map: gpd.GeoDataFrame, _collectors: pd.DataFrame, first_app
     start_day = _collectors['Data_1o_Esporos'].iloc[0]
 
     for i in range(len(first_apperances)):
-        infection_circle = Point(first_apperances['Longitude'].iloc[i], first_apperances['Latitude'].iloc[i])
-        infection_circle.buffer(TEST_PARAMS['buffer_factor'])
-        infection_circles.append(Infection_Circle(infection_circle, 1))
+
+        infection_circle = Infection_Circle(
+            Point(first_apperances['Longitude'].iloc[i], first_apperances['Latitude'].iloc[i]),
+            1,
+            start_day
+        )
+
+        infection_circles.append(infection_circle)
 
     for day in range(TEST_PARAMS['number_of_days']):
 
@@ -91,14 +65,18 @@ def circular_growth(_map: gpd.GeoDataFrame, _collectors: pd.DataFrame, first_app
 
                                 _collectors.loc[collector.Index, 'color'] = 'red'
 
-                            check_day(_collectors, start_day + datetime.timedelta(day), collector, TEST_PARAMS)
+                            check_day(collector, start_day + datetime.timedelta(day))
 
-                            new_infection_circle = Point(collector.Longitude, collector.Latitude)
-                            new_infection_circle.buffer(TEST_PARAMS['buffer_factor'])
-                            infection_circles.append(Infection_Circle(new_infection_circle, 1))
+                            new_infection_circle = Infection_Circle(
+                                Point(collector.Longitude, collector.Latitude),
+                                1, 
+                                start_day + datetime.timedelta(day)
+                            )
+
+                            infection_circles.append(new_infection_circle)
         
         for infection_circle in infection_circles:
-            infection_circle.grow(TEST_PARAMS['buffer_factor'], day)
+            infection_circle.grow(gf.logarithmic_growth_distance)
 
         if day == TEST_PARAMS['number_of_days'] - 1:
             plots.plotting(_map, _collectors, infection_circles, old_circles, start_day, day)
