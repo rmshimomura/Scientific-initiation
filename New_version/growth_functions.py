ff1ad4fa444823f22ca220f00b0d2b6d330e18b8
rmshimomura <shimomurarodrigo1@gmail.com>
2023-02-06 21:22:52 -0300

    Add some growths

--- New_version/growth_functions.py
+++ New_version/growth_functions.py
@@ -1,7 +1,25 @@
 import math
 
-def logarithmic_growth_distance(day: int) -> float:
+def base_7_logarithmic_growth_distance(day: int) -> float:
     return math.log(day, 7)
 
-def logarithmic_growth_days(distance: float) -> float:
+def base_7_logarithmic_growth_days(distance: float) -> float:
     return 7**distance
+
+def base_10_logarithmic_growth_distance(day: int) -> float:
+    return math.log(day, 10)
+
+def base_10_logarithmic_growth_days(distance: float) -> float:
+    return 10**distance
+
+def base_100_logarithmic_growth_distance(day: int) -> float:
+    return math.log(day, 100)
+
+def base_100_logarithmic_growth_days(distance: float) -> float:
+    return 100**distance
+
+def base_50_div_2_logarithmic_growth_distance(day: int) -> float:
+    return math.log(day, 50)/2
+
+def base_50_div_2_logarithmic_growth_days(distance: float) -> float:
+    return 50**(distance*2)
