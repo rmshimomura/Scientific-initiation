a67d8393d24903b5d791c2d826a262b05cc95e9e
rmshimomura <shimomurarodrigo1@gmail.com>
2023-02-06 21:24:09 -0300

    Add new penalties

--- New_version/main.py
+++ New_version/main.py
@@ -2,16 +2,19 @@ import geopandas as gpd
 import pandas as pd
 import utils, time, math, datetime
 import growth_types as gt
+import growth_functions as gf
 
 def read_basic_info():
     # Read map file
-    _map = gpd.read_file('G:/Meu Drive/IC/Codes/Data/Maps/PR_Municipios_2021/PR_Municipios_2021.shp') 
+    _map = gpd.read_file('G:/My Drive/IC/Codes/Data/Maps/PR_Municipios_2021/PR_Municipios_2021.shp') 
 
     # Read collectors file and sort them based on the date of the first spores
-    _collectors = pd.read_csv('G:/Meu Drive/IC/Codes/Data/Collectors/2021/ColetoresSafra2122.csv', sep=';', decimal=',', parse_dates=['Data_1o_Esporos'], infer_datetime_format=True)
+    _collectors = pd.read_csv('G:/My Drive/IC/Codes/Data/Collectors/2021/ColetoresSafra2122.csv', sep=';', decimal=',', parse_dates=['Data_1o_Esporos'], infer_datetime_format=True)
     _collectors = _collectors.sort_values(by=['Data_1o_Esporos'])
             
     _collectors = utils.clean_up(_collectors)
+
+    _collectors['discovery_day'] = None
 
     return _map, _collectors
 
@@ -33,8 +36,6 @@ def coloring_collectors(_collectors):
 
 TEST_PARAMS = {
     'number_of_days' : 200,
-    'new_seed_threshold' : 50,
-    'collector_penalty' : 50,
 }
 
 _map, _collectors = read_basic_info()
@@ -45,9 +46,22 @@ number_of_valid_collectors = utils.count_valid_collectors(_collectors)
 
 coloring_collectors(_collectors)
 
-error_value, infection_circles, invalid_collectors = gt.circular_growth(_map, _collectors, first_apperances, old_circles, TEST_PARAMS)
+true_positive_penalty, infection_circles = gt.circular_growth(_map, _collectors, first_apperances, old_circles, TEST_PARAMS, gf.base_50_div_2_logarithmic_growth_distance)
+
+true_negative_penalty = 0
+
+false_positive_penalty = utils.calculate_false_positives_penalty(_collectors, start_day + datetime.timedelta(days=TEST_PARAMS['number_of_days'] - 1))
+
+false_negative_penalty = utils.calculate_false_negatives_penalty(_collectors, gf.base_50_div_2_logarithmic_growth_days)
+
+total_error = true_positive_penalty + true_negative_penalty + false_positive_penalty + false_negative_penalty
 
 print(f"TEST PARAMS: {TEST_PARAMS}")
-print(f"Total error (considering only valid collectors): {math.sqrt(error_value/number_of_valid_collectors)}")
-print(f"False positives percentage: {invalid_collectors/len(_collectors)*100}%")
-print(f"Discovered collectors percentage: {len(infection_circles)/len(_collectors)*100}%")
+print(f"True positive penalty: {true_positive_penalty}")
+print(f"True negative penalty: {true_negative_penalty}")
+print(f"False positive penalty: {false_positive_penalty}")
+print(f"False negative penalty: {false_negative_penalty}")
+print(f"Total error {math.sqrt(total_error/len(_collectors))}\n\n")
+
+for i in range(len(infection_circles)):
+    print(f"Circle {i} has {infection_circles[i].buffer * 111.045} km of radius and {infection_circles[i].life_span} life span")
--- New_version/utils.py
+++ New_version/utils.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import datetime
+from shapely.geometry import Point
+import growth_functions as gf
 
 def clean_up(_collectors: pd.DataFrame)-> pd.DataFrame: 
     # Reset index
@@ -31,3 +34,47 @@ def count_valid_collectors(_collectors: pd.DataFrame)-> int:
             number_of_valid_collectors += 1
 
     return number_of_valid_collectors
+
+def calculate_false_positives_penalty(_collectors: pd.DataFrame, final_day: datetime.timedelta)-> int:
+    
+    penalty = 0
+
+    for i in range(0, len(_collectors)):
+        if pd.isnull(_collectors['Data_1o_Esporos'].iloc[i]) and _collectors['Detected'].iloc[i] == 1:
+            penalty += (abs(final_day.day - _collectors['discovery_day'].iloc[i].day) ** 2)
+
+    return penalty
+
+def calculate_false_negatives_penalty(_collectors: pd.DataFrame, growth_function)-> int:
+
+    penalty = 0
+
+    not_detected_collectors = _collectors[_collectors['Detected'] == 0]
+
+    for i in range(0, len(not_detected_collectors)):
+
+        distance_to_reach = find_closest_positive_collector(_collectors, not_detected_collectors.iloc[i])
+
+        days_to_reach = growth_function(distance_to_reach)
+
+        penalty += abs(days_to_reach ** 2)
+
+    return penalty
+
+def find_closest_positive_collector(_collectors: pd.DataFrame, collector: pd.DataFrame)-> pd.DataFrame:
+
+    collectors_with_spores = _collectors[_collectors['Situacao'] == 'Com esporos']
+
+    closest_collector = collectors_with_spores.iloc[0]
+    closest_point = Point(closest_collector['Longitude'], closest_collector['Latitude'])
+    collector_point = Point(collector['Longitude'], collector['Latitude'])
+
+    for i in range(0, len(collectors_with_spores)):
+
+        iterator_point = Point(collectors_with_spores.iloc[i]['Longitude'], collectors_with_spores.iloc[i]['Latitude'])
+
+        if iterator_point.distance(collector_point) < closest_point.distance(collector_point):
+            closest_point = iterator_point
+            closest_collector = collectors_with_spores.iloc[i]
+
+    return closest_point.distance(collector_point)
