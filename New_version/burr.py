b97b593f01923ce8e6d7710b4900d6d78a560abd
rmshimomura <shimomurarodrigo1@gmail.com>
2023-03-27 19:05:18 -0300

    Burr class creation

---
+++ New_version/burr.py
@@ -0,0 +1,13 @@
+class Burr:
+
+    def __init__(self, geometry, buffer, discovery_day):
+        self.geometry = geometry
+        self.buffer = buffer
+        self.life_span = 0.001
+        self.discovery_day = discovery_day
+    def grow(self, growth_function, base):
+
+        if self.life_span < 105:
+            self.life_span += 1
+            self.buffer = growth_function(self.life_span, base)
+            self.geometry = self.geometry.buffer(self.buffer)
