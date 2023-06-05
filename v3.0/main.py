96b7d84fc8e3f722151f170ac0916068802238e3
rmshimomura <shimomurarodrigo1@gmail.com>
2023-06-05 18:01:12 -0300

    Cleanup and modularization

--- v3.0/main.py
+++ v3.0/main.py
@@ -5,8 +5,6 @@ import growth_functions as gf
 import matplotlib.pyplot as plt
 import os
 import shapely.geometry as sg
-import shapely.ops
-import datetime
 
 root_folder = None
 
@@ -61,91 +59,6 @@ def coloring_collectors(_collectors):
     _collectors.loc[_collectors['Primeiro_Esporo'].notnull(), 'color'] = 'green'
     _collectors.loc[_collectors['Primeiro_Esporo'].isnull(), 'color'] = 'red'
 
-def grid_region(_map, _collectors, burr_buffer):
-
-    global file_name, year_analyzed, horizontal_len, vertical_len
-
-    # horizontal_len = math.ceil(total_length / 0.1)
-    # vertical_len = math.ceil(total_height / 0.1)
-    horizontal_len = 31
-    vertical_len = 23
-
-    new_points_data = open(f"G:/{root_folder}/IC/Codes/v3.0/new_points/{file_name}_{horizontal_len}_{vertical_len}.csv", 'w')
-
-    new_points_data.write('Mesorregiao,Regiao,Municipio,Produtor,Cultivar,LatitudeDecimal,LongitudeDecimal,Primeiro_Esporo,Estadio Fenologico,Situacao,Dias_apos_O0,InicioCiclo,DiasAposInicioCiclo\n')
-
-    min_point = _map.total_bounds[0:2]
-    max_point = _map.total_bounds[2:4]
-    total_length = max_point[0] - min_point[0]
-    total_height = max_point[1] - min_point[1]
-
-    boxes = []
-
-    map_exterior_geometry = shapely.ops.unary_union(_map.geometry).buffer(0.2)
-
-    ## Create a datetime date for september 10th with year = year_analyzed
-    september_10th = datetime.datetime(year_analyzed, 9, 10)
-
-    for i in range(horizontal_len):
-        for j in range(vertical_len):
-            new_box = sg.box(min_point[0] + (total_length * i / horizontal_len),
-                            min_point[1] + (total_height * j / vertical_len),
-                            min_point[0] + (total_length * (i + 1) / horizontal_len),
-                            min_point[1] + (total_height * (j + 1) / vertical_len)
-                            )            
-            if map_exterior_geometry.contains(new_box):
-                boxes.append(new_box)
-
-    for box in boxes:
-        
-        # Pick all points inside the box
-        points_inside_box = []
-        for i in _collectors.itertuples():
-            collector_point = sg.Point(i.LongitudeDecimal, i.LatitudeDecimal)
-            if box.contains(collector_point) or box.touches(collector_point):
-                if not pd.isna(i.Primeiro_Esporo):
-
-                    points_inside_box.append(i)
-
-        first_apperance_day = None
-
-        center_point = {
-            'LongitudeDecimal': box.centroid.x,
-            'LatitudeDecimal': box.centroid.y,
-            'Primeiro_Esporo': None,
-            'Situacao': 'Encerrado sem esporos'
-        }
-
-        # Search for the earliest apperance day
-        for point in points_inside_box:
-
-            if point.Primeiro_Esporo is not None and not pd.isnull(point.Primeiro_Esporo):
-
-                if first_apperance_day is None:
-
-                    first_apperance_day = point.Primeiro_Esporo    
-                    center_point['Situacao'] = point.Situacao
-
-                elif first_apperance_day > point.Primeiro_Esporo:
-
-                    first_apperance_day = point.Primeiro_Esporo
-
-        if first_apperance_day is not None:
-
-            center_point['Primeiro_Esporo'] = first_apperance_day
-
-        # If any point detected a spore
-        if center_point['Primeiro_Esporo'] is not None:
-
-            days_differ = (center_point['Primeiro_Esporo'] - september_10th).days
-
-            new_points_data.write(f",,,,,{center_point['LatitudeDecimal']},{center_point['LongitudeDecimal']},{center_point['Primeiro_Esporo'].strftime('%d/%m/%y')},,{center_point['Situacao']},,{september_10th.strftime('%d/%m/%y')},{days_differ}\n")
-        
-        # If no point detected a spore
-        else:
-            
-            new_points_data.write(f",,,,,{center_point['LatitudeDecimal']},{center_point['LongitudeDecimal']},,,{center_point['Situacao']},,{september_10th.strftime('%d/%m/%y')},\n")
-    
 def add_fake_collectors(_collectors):
 
     fake_collectors_file = open('G:/' + root_folder + '/IC/Codes/Data/Collectors/2021/Fake_Collectors.csv', 'w')
@@ -191,7 +104,7 @@ if TEST_PARAMS['Fake_Collectors']:
 
 method_used = None
 
-grid_region(_map, _collectors, burr_buffer)
+utils.grid_region(_map, _collectors, root_folder, file_name, year_analyzed)
 
 # true_positive_penalty, infection_circles, method_used = \
 #     gt.circular_growth(_map, _collectors, first_apperances, old_geometries, TEST_PARAMS)
--- v3.0/utils.py
+++ v3.0/utils.py
@@ -6,6 +6,8 @@ import growth_functions as gf
 import math
 import geopandas as gpd
 import os
+import shapely
+import shapely.geometry as sg
 
 def write_csv(TEST_PARAMS: dict, PENALTIES: dict, start_day, end_day, method_used)-> None:
 
@@ -172,3 +174,87 @@ def treat_position(origin, point_found, c_radius):
         result_point.append(point_found.y + abs((math.sin(math.atan(slope)) * c_radius)))
     
     return Point(result_point[0], result_point[1])
+
+def grid_region(_map, _collectors, root_folder, file_name, year_analyzed):
+
+    # horizontal_len = math.ceil(total_length / 0.1)
+    # vertical_len = math.ceil(total_height / 0.1)
+    horizontal_len = 31
+    vertical_len = 23
+
+    new_points_data = open(f"G:/{root_folder}/IC/Codes/v3.0/new_points/{file_name}_{horizontal_len}_{vertical_len}.csv", 'w')
+
+    new_points_data.write('LatitudeDecimal,LongitudeDecimal,Primeiro_Esporo,Situacao,InicioCiclo,DiasAposInicioCiclo\n')
+
+    min_point = _map.total_bounds[0:2]
+    max_point = _map.total_bounds[2:4]
+    total_length = max_point[0] - min_point[0]
+    total_height = max_point[1] - min_point[1]
+
+    boxes = []
+
+    map_exterior_geometry = shapely.ops.unary_union(_map.geometry).buffer(0.2)
+
+    ## Create a datetime date for september 10th with year = year_analyzed
+    september_10th = datetime.datetime(year_analyzed, 9, 10)
+
+    for i in range(horizontal_len):
+        for j in range(vertical_len):
+            new_box = sg.box(min_point[0] + (total_length * i / horizontal_len),
+                            min_point[1] + (total_height * j / vertical_len),
+                            min_point[0] + (total_length * (i + 1) / horizontal_len),
+                            min_point[1] + (total_height * (j + 1) / vertical_len)
+                            )            
+            if map_exterior_geometry.contains(new_box):
+                boxes.append(new_box)
+
+    for box in boxes:
+        
+        # Pick all points inside the box
+        points_inside_box = []
+        for i in _collectors.itertuples():
+            collector_point = sg.Point(i.LongitudeDecimal, i.LatitudeDecimal)
+            if box.contains(collector_point) or box.touches(collector_point):
+                if not pd.isna(i.Primeiro_Esporo):
+
+                    points_inside_box.append(i)
+
+        first_apperance_day = None
+
+        center_point = {
+            'LongitudeDecimal': box.centroid.x,
+            'LatitudeDecimal': box.centroid.y,
+            'Primeiro_Esporo': None,
+            'Situacao': 'Encerrado sem esporos'
+        }
+
+        # Search for the earliest apperance day
+        for point in points_inside_box:
+
+            if point.Primeiro_Esporo is not None and not pd.isnull(point.Primeiro_Esporo):
+
+                if first_apperance_day is None:
+
+                    first_apperance_day = point.Primeiro_Esporo    
+                    center_point['Situacao'] = point.Situacao
+
+                elif first_apperance_day > point.Primeiro_Esporo:
+
+                    first_apperance_day = point.Primeiro_Esporo
+
+        if first_apperance_day is not None:
+
+            center_point['Primeiro_Esporo'] = first_apperance_day
+
+        # If any point detected a spore
+        if center_point['Primeiro_Esporo'] is not None:
+
+            days_differ = (center_point['Primeiro_Esporo'] - september_10th).days
+
+            new_points_data.write(f"{center_point['LatitudeDecimal']},{center_point['LongitudeDecimal']},{center_point['Primeiro_Esporo'].strftime('%d/%m/%y')},{center_point['Situacao']},{september_10th.strftime('%d/%m/%y')},{days_differ}\n")
+        
+        # If no point detected a spore
+        else:
+            
+            new_points_data.write(f"{center_point['LatitudeDecimal']},{center_point['LongitudeDecimal']},,{center_point['Situacao']},{september_10th.strftime('%d/%m/%y')},\n")
+    
