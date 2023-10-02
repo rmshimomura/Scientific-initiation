a64fcb9419c004753b740bfa7a6cfcb7fc3bd2fd
rmshimomura <shimomurarodrigo1@gmail.com>
2023-10-02 11:08:16 -0300

    Wrong data

--- Results/Growth_tests/defined_bases/tabelas.py
+++ Results/Growth_tests/defined_bases/tabelas.py
@@ -2,10 +2,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-CGNT_DATA = pd.read_csv(r'G:\My Drive\IC\Codes\Results\Growth_tests\defined_bases\CGNT.csv')
-CGT_DATA = pd.read_csv(r'G:\My Drive\IC\Codes\Results\Growth_tests\defined_bases\CGT.csv')
-MG_DATA = pd.read_csv(r'G:\My Drive\IC\Codes\Results\Growth_tests\defined_bases\MG.csv')
-TG_DATA = pd.read_csv(r'G:\My Drive\IC\Codes\Results\Growth_tests\defined_bases\TG.csv')
+CGNT_DATA = pd.read_csv(r'G:\Meu Drive\IC\Codes\Results\Growth_tests\defined_bases\CGNT.csv')
+CGT_DATA = pd.read_csv(r'G:\Meu Drive\IC\Codes\Results\Growth_tests\defined_bases\CGT.csv')
+MG_DATA = pd.read_csv(r'G:\Meu Drive\IC\Codes\Results\Growth_tests\defined_bases\MG.csv')
+TG_DATA = pd.read_csv(r'G:\Meu Drive\IC\Codes\Results\Growth_tests\defined_bases\TG.csv')
 
 # Unite all dataframes
 ALL_DATA = pd.concat([CGNT_DATA, CGT_DATA, MG_DATA, TG_DATA])
@@ -16,7 +16,7 @@ total_collectors = 504
 harvests_true_positives = [126,68,98]
 harvests_false_positives = [total_collectors - i for i in harvests_true_positives]
 
-harvest = '2021'
+harvest = '2223'
 
 ALL_DATA['active'] = 1
 all_data_harvest = ALL_DATA.query(f"test_file == 'coletoressafra{harvest}_31_23' and (train_file == 'arithmetic_mean_31_23' or growth_type == 'CGT')")
@@ -38,7 +38,7 @@ all_data_harvest = all_data_harvest.query("active == 1")
 
 all_data_harvest = all_data_harvest.query('not_detected_with_spores == 0')
 
-all_data_harvest['biggest_error'] = np.maximum(abs(all_data_harvest['days_error_mean_total'] + all_data_harvest['days_error_std_total']), abs(all_data_harvest['days_error_mean_total'] - all_data_harvest['days_error_std_total']))
+all_data_harvest['error_max'] = np.maximum(abs(all_data_harvest['days_error_mean_total'] + all_data_harvest['days_error_std_total']), abs(all_data_harvest['days_error_mean_total'] - all_data_harvest['days_error_std_total']))
 
 chosen_harvest_values = (harvests_true_positives[all_harversts.index(harvest)], harvests_false_positives[all_harversts.index(harvest)])
 
@@ -47,9 +47,9 @@ all_data_harvest['recall'] = all_data_harvest['true_positive'] / chosen_harvest_
 all_data_harvest['f1_score'] = 2 * (all_data_harvest['precision'] * all_data_harvest['recall']) / (all_data_harvest['precision'] + all_data_harvest['recall'])
 all_data_harvest['accuracy'] = (all_data_harvest['true_positive'] + (total_collectors - chosen_harvest_values[0])) / total_collectors
 
-all_data_harvest = all_data_harvest.sort_values(by=['f1_score', 'biggest_error'], key=abs, ascending=[False, True])
+all_data_harvest = all_data_harvest.sort_values(by=['error_max'], key=abs, ascending=[True])
 all_data_harvest = all_data_harvest.round(3)
-best_10 = all_data_harvest[['growth_type', 'radius', 'train_file', 'days_error_mean_total', 'days_error_std_total', 'true_positive', 'false_positive', 'biggest_error','f1_score']].head(30)
+best_10 = all_data_harvest[['growth_type', 'radius', 'train_file', 'days_error_mean_total', 'days_error_std_total', 'true_positive', 'false_positive', 'error_max','f1_score']].head(10)
 
 print(f"{harvest}\n\n")
 
@@ -57,4 +57,4 @@ print(best_10)
 print("=========================================")
 
 # Export to csv best_10
-# best_10.to_csv(rf'G:\My Drive\IC\Codes\Results\Growth_tests\defined_bases\f1_score_best_10_{harvest}.csv', index=False)
+best_10.to_csv(rf'G:\Meu Drive\IC\Codes\Results\Growth_tests\defined_bases\error_max_best_10_{harvest}.csv', index=False)
